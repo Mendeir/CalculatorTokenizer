1e18d3a3223cb178c6a78a8d181202bc1d395555
Lorenze Dionell K. Arroyo <90163665+lorenzeArroyo@users.noreply.github.com>
2022-05-16 09:49:38 +0800

    Update Position.java -> added a constructor

--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,12 @@
 public class Position {
     private int idx, ln, col, fn;
     private String ftxt;
+
+    public Position(int idx, int ln, int col, int fn, String ftxt){
+        this.idx = idx;
+        this.ln = ln;
+        this.col = col;
+        this.fn = fn;
+        this.ftxt = ftxt;
+    }
 }
