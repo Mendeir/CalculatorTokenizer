7523540d9c73cf43d2f83bd63dc94099f861b805
MG <mg-git.plumbic@8shield.net>
2022-05-16 14:35:09 +0800

    Implement number data type checker
    Errors are to be found since Token class is not yet implemented.

--- src/Lexer.java
+++ src/Lexer.java
@@ -10,4 +10,26 @@ public class Lexer {
         this.position = position;
         this.current_char = current_char;
     }
+
+    public Token makeNumber() {
+        String number_string = " ";
+        int dot_count = 0;
+
+        while (current_char != " ") {
+            if (current_char == ".") {
+                if (dot_count == 1)
+                    break;
+                dot_count += 1;
+                number_string += ".";
+            } else {
+                number_string += current_char;
+            }
+
+            if (dot_count == 0) {
+                return Token(TT_INT, int(number_string));
+            } else {
+                return Token(TT_FLOAT, float(number_string));
+            }
+        }
+    }
 }
