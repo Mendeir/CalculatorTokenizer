ea2e07f2e2b9c74bf2ec0a5cb039aedeea543ac8
dbornilla <78960805+dbornilla@users.noreply.github.com>
2022-05-17 21:02:01 +0800

    Error part Three convertion to java

--- src/Error.java
+++ src/Error.java
@@ -1,12 +1,12 @@
 public class Error {
 
-    private int pos_start;
-    private int pos_end;
+    private Position pos_start;
+    private Position pos_end;
     private String error_name;
     private String details;
 
     // constructor
-    Error(int pos_start,int pos_end,String error_name,String details){
+    Error(Position pos_start,Position pos_end,String error_name,String details){
         this.pos_start = pos_start;
         this.pos_end = pos_end;
         this.error_name = error_name;
@@ -15,7 +15,8 @@ public class Error {
     // method to evaluate string
     public String as_string(){
         String result;
-        result = this.error_name + ":" + this.details;
+        result = this.error_name + ":" + this.details + "\n";
+        result += "File" + this.pos_start.getFn() + "," + "line" + this.pos_start.getLn();
         return result;
     }
 }
