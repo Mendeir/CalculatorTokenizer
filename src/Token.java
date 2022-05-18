1cd77b1452a202740247c65a49ad509502955088
Allen <allendelarosa456@gmail.com>
2022-05-18 21:15:25 +0800

    Added Class Documentation
    Added Class Documentation

--- src/Token.java
+++ src/Token.java
@@ -1,9 +1,12 @@
+//this class contains methods that allows different token types
+//to be arranged in a string to make tokens in a Type:Value format
 public class Token{
+
     static String returnToken(String type, String value){
 
         return String.format("%1$s:%2$s", type, value);
     }
-    
+
     static String returnToken(String type, int value){
 
         return String.format("%1$s:%2$s", type, value);
@@ -13,6 +16,7 @@ public class Token{
 
         return String.format("%1$s:%2$s", type, value);
     }
+
     static String returnToken(String type){
         return type;
     }
